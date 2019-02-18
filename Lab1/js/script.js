a036bfd11bb80029c3b4bd792b81e86c856d4543
paveleroshkinweb <eroshikn@mail.ru>
2019-02-18 07:26:32 +0300

    add lab 1

---
+++ Lab1/js/script.js
@@ -0,0 +1,116 @@
+const canvas1 = document.getElementById('task1');
+const ctx1 = canvas1.getContext('2d');
+const canvas2 = document.getElementById('task2');
+const ctx2 = canvas2.getContext('2d');
+const canvas3 = document.getElementById('task3');
+const ctx3 = canvas3.getContext('2d');
+
+function drawLine(ctx, point1, point2, point1Name, point2Name) {
+    ctx.moveTo(point1.x, point1.y);
+    ctx.lineTo(point2.x, point2.y);
+    ctx.strokeText(point1Name, point1.x - 3, point1.y - 3);
+    ctx.strokeText(point2Name, point2.x - 3, point2.y - 3);
+    ctx.stroke();
+}
+
+function drawPoint(ctx, point){
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI, true);
+    ctx.strokeText('p0', point.x - 5, point.y - 5);
+    ctx.fill();
+}
+
+function randomPoint(width, height) {
+    return {
+        x: Math.ceil(Math.random() * width),
+        y: Math.ceil(Math.random() * height)
+    };
+}
+
+function createPoint(x, y) {
+    return { x, y };
+}
+
+function formMatrix(p1, p2, p3) {
+    return [
+        [p3.x - p2.x, p3.y - p2.y],
+        [p1.x - p2.x, p1.y - p2.y]
+    ];
+}
+
+
+function checkLocation(point, beginLine, endLine) {
+    const det = math.det(formMatrix(point, beginLine, endLine));
+    if (det > 0) {
+        return 'to the right';
+    }
+    else if (det < 0) {
+        return 'to the left';
+    }
+    else {
+        return 'belong the line'
+    }
+}
+
+function writeMessage(content, container) {
+    const div = document.createElement('div');
+    div.textContent = content;
+    div.classList.add('message');
+    container.appendChild(div);
+}
+
+function drawPolygon(ctx, points) {
+    for (let i = 0; i < points.length; i++) {
+        if (i === points.length - 1) drawLine(ctx, points[i], points[0], `p${i+1}`, 'p1');
+        else drawLine(ctx, points[i], points[i+1], `p${i+1}`, `p${i+2}`);
+    }
+}
+
+function checkPolygon(points) {
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i + 2; j < points.length - 1; j++) {
+
+        }
+    }
+}
+
+function solveTask1() {
+    const p1 = randomPoint(canvas1.width, canvas1.height);
+    const p2 = randomPoint(canvas1.width, canvas1.height);
+    const p0 = randomPoint(canvas1.width, canvas1.height);
+    drawPoint(ctx1, p0);
+    drawLine(ctx1, p1, p2, 'p1', 'p2');
+    const pointLocation = checkLocation(p0, p1, p2);
+    writeMessage(`point p0 ${pointLocation} according the line p1 p2`, document.querySelector('.container1'));
+}
+
+function solveTask2() {
+    const p1 = randomPoint(canvas2.width, canvas2.height);
+    const p2 = randomPoint(canvas2.width, canvas2.height);
+    const p3 = randomPoint(canvas2.width, canvas2.height);
+    const p4 = randomPoint(canvas2.width, canvas2.height);
+    drawLine(ctx2, p1, p2, 'p1', 'p2');
+    drawLine(ctx2, p3, p4, 'p3', 'p4');
+    const d1 =  math.det(formMatrix(p1, p3, p4));
+    const d2 =  math.det(formMatrix(p2, p3, p4));
+    const d3 =  math.det(formMatrix(p3, p1, p2));
+    const d4 =  math.det(formMatrix(p4, p1, p2));
+    let  message = "This lines are not intersect";
+    if (d1 * d2 <= 0 && d3 * d4 <= 0) message = "The lines are intersect";
+    else if (d1 == d2 && d2 == d3 && d3 == d4) message = "The lines are match up";
+    writeMessage(message, document.querySelector('.container2'));
+}
+
+function solveTask3() {
+    let points = [];
+    for (let i = 0; i < 5; i++) {
+        points.push(randomPoint(canvas3.width, canvas3.height));
+    }
+    drawPolygon(ctx3, points);
+    //checkPolygon(points);
+}
+
+solveTask1();
+solveTask2();
+solveTask3();
+//solveTask4();
